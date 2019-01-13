2135789aed3423784993bfcf038da4cda112b205
dagilleland <dagilleland@users.noreply.github.com>
2019-01-13 16:28:31 -0700

    Closes #1

--- src/WestWind/dbo/Tables/ManifestItems.sql
+++ src/WestWind/dbo/Tables/ManifestItems.sql
@@ -3,5 +3,7 @@
     [ManifestItemID] INT NOT NULL PRIMARY KEY IDENTITY, 
     [ShipmentID] INT NOT NULL, 
     [ProductID] INT NOT NULL, 
-    [ShipQuantity] SMALLINT NOT NULL
+    [ShipQuantity] SMALLINT NOT NULL, 
+    CONSTRAINT [FK_ManifestItems_ToShipments] FOREIGN KEY ([ShipmentID]) REFERENCES [Shipments]([ShipmentID]), 
+    CONSTRAINT [FK_ManifestItems_ToProducts] FOREIGN KEY ([ProductID]) REFERENCES [Products]([ProductID])
 )
--- src/WestWind/dbo/Tables/Shipments.sql
+++ src/WestWind/dbo/Tables/Shipments.sql
@@ -5,5 +5,7 @@
     [ShippedDate] DATETIME NOT NULL, 
     [ShipVia] INT NOT NULL, 
     [FreightCharge] MONEY NOT NULL, 
-    [TrackingCode] VARCHAR(128) NULL
+    [TrackingCode] VARCHAR(128) NULL, 
+    CONSTRAINT [FK_Shipments_ToTShippers] FOREIGN KEY ([ShipVia]) REFERENCES [Shippers]([ShipperID]), 
+    CONSTRAINT [FK_Shipments_ToOrders] FOREIGN KEY ([OrderID]) REFERENCES [Orders]([OrderID])
 )
