af3ee544b1cf49b10f983c36be274a0faee7d05d
dagilleland <dagilleland@users.noreply.github.com>
2019-03-13 21:09:23 -0600

    Tidy up checks to prevent duplicate updates.

---
+++ src/WestWind.DataPopulation/DateShifting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using WestWind.DataPopulation.Db;
+
+namespace WestWind.DataPopulation
+{
+    public class DateShifting
+    {
+        public static void CorrectForWeekdays()
+        {
+            using (var context = new WestWindContext())
+            {
+                int offset = 0;
+                var info = context.Orders.Where(x => x.OrderDate.HasValue).Select(x => x.OrderDate.Value).ToList();
+                if (info.Any(x => (x.DayOfWeek == DayOfWeek.Saturday || x.DayOfWeek == DayOfWeek.Sunday)))
+                {
+                    offset = info.Count(x => x.DayOfWeek == DayOfWeek.Saturday) == 0 ? 1
+                           : info.Count(x => x.DayOfWeek == DayOfWeek.Friday) == 0 ? 2
+                           : info.Count(x => x.DayOfWeek == DayOfWeek.Thursday) == 0 ? 3
+                           : info.Count(x => x.DayOfWeek == DayOfWeek.Wednesday) == 0 ? 4
+                           : info.Count(x => x.DayOfWeek == DayOfWeek.Tuesday) == 0 ? 5
+                           : 6;
+                    var orders = context.Orders;
+                    foreach (var order in orders)
+                        order.OrderDate = order.OrderDate.Value.AddDays(offset);
+                    context.SaveChanges();
+                    Console.WriteLine("Fixed order dates");
+                }
+
+            }
+        }
+    }
+}
---
+++ src/WestWind.DataPopulation/Invoicing.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using WestWind.DataPopulation.Db;
+using WestWind.DataPopulation.Db.Views;
+
+namespace WestWind.DataPopulation
+{
+    public class Invoicing
+    {
+        public static void PayInvoices()
+        {
+            // Policy of setting the payment due date to at least 28 days after the order date, unless the required date is longer than 28 days away. In that case, we will add a week at a time until the required date is passed.
+            SetPaymentDueDate();
+            MakePayments();
+        }
+        static Random Rnd = new Random();
+        static void MakePayments()
+        {
+            using (var context = new WestWindContext())
+            {
+                if (!context.Payments.Any())
+                {
+                    context.Database.ExecuteSqlCommand("ALTER TABLE Payments NOCHECK CONSTRAINT [CK_PAY_PDate_Not_Old]");
+                    var subtotals = context.Database.SqlQuery<OrderSubtotal>(OrderSubtotal.Query);
+                    var methods = context.PaymentTypes.Select(x => x.PaymentTypeID).ToList();
+                    foreach (var order in subtotals)
+                    {
+                        var ord = context.Orders.Find(order.OrderID);
+                        if (ord.Shipped)
+                        {
+                            // Make payment(s)
+                            var amountDue = order.Subtotal + (ord.Freight.HasValue ? ord.Freight.Value : 0m);
+                            var date = ord.PaymentDueDate.Value.AddDays(Rnd.Next(4) * -1);
+                            var method = methods[Rnd.Next(methods.Count)];
+
+                            var pay = new Payment
+                            {
+                                Amount = amountDue,
+                                OrderID = order.OrderID,
+                                PaymentDate = date,
+                                PaymentTypeID = method,
+                                TransactionID = Guid.NewGuid()
+                            };
+                            if (date.AddDays(14) < DateTime.Today)
+                                pay.ClearedDate = date.AddDays(Rnd.Next(3, 14));
+                            context.Payments.Add(pay);
+                            Console.WriteLine($"Paying order {pay.OrderID} - {pay.Amount:C}");
+                        }
+                    }
+                    context.SaveChanges();
+                    context.Database.ExecuteSqlCommand("ALTER TABLE Payments CHECK CONSTRAINT [CK_PAY_PDate_Not_Old]");
+                    context.SaveChanges();
+                }
+            }
+        }
+        static void SetPaymentDueDate()
+        {
+            using (var context = new WestWindContext())
+            {
+                if (!context.Payments.Any())
+                {
+                    foreach (var order in context.Orders)
+                    {
+                        if (order.OrderDate.HasValue && order.RequiredDate.HasValue)
+                        {
+                            var due = order.OrderDate.Value.AddDays(28);
+                            while (due < order.RequiredDate) due = due.AddDays(7);
+                            order.PaymentDueDate = due;
+                        }
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
